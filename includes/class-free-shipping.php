fc16615db511bb065557408d04da2b94c2077f7d
Samiullah <samiullah954015@gmail.com>
2026-05-11 16:41:28 +0500

    done

--- includes/class-free-shipping.php
+++ includes/class-free-shipping.php
@@ -31,7 +31,7 @@ class ItchenKing_Free_Shipping {
      */
     public static function get_threshold() {
 
-        $fallback = 5000;
+        $fallback = 50;
         $amounts  = [];
 
         if (class_exists('WC_Shipping_Zones')) {
